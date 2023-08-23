feccb3e1647caac3773a12e3f44e9caa19ae4b3e
KavinduWeerasinghe <75959908+KavinduWeerasinghe@users.noreply.github.com>
2023-08-23 19:03:36 +0530

    wavelets

--- read.py
+++ read.py
@@ -13,15 +13,27 @@ def mov_avg_filt(y,win=4):
     ret= np.pad(ret, (0,2), 'constant')
     return ret
 
-def bandpass_filt(Y):
-    sos = signal.cheby1(2, 1, Wn=[0.5,40], btype='bandpass', fs=512, output='sos')
+def bandpass_filt(Y,freq1=0.5,freq2=100):
+    sos = signal.cheby1(2, 1, Wn=[freq1,freq2], btype='bandpass', fs=512, output='sos')
     filtered = signal.sosfilt(sos, Y)
     return filtered
 
-def show_wave(r,samples,):
-    axs.plot(samples,r)
+def make_bands(Y):
+    #delta
+    delta = bandpass_filt(Y,1,4)
+    theta = bandpass_filt(Y,4,8)
+    alpha = bandpass_filt(Y,8,13)
+    beta = bandpass_filt(Y,13,30)
+    gamma = bandpass_filt(Y,30,100)
+    return delta,theta,alpha,beta,gamma
 
-def open_record(rec_num : str)->tuple:
+def show_wave(r,samples,multiple=False):
+    if multiple>=0:
+        axs[multiple].plot(samples,r)
+    else:
+        axs.plot(samples,r)
+        
+def open_record(rec_fol: str, rec_num : str)->tuple:
     """This function opens a given recording in NST database.
 
     :param rec_num: The number/name combination of NST databse
@@ -35,12 +47,12 @@ def open_record(rec_num : str)->tuple:
         
     database location : D:/Intership/Template matching/databases/data_NST/
     """
-    data=mne.io.read_raw_edf("F:/FYP/scalp dataset/PN00/{0}.edf".format(rec_num))
+    data=mne.io.read_raw_edf("F:/FYP/Sienna Scalp dataset/files/sienna-scalp-eeg/1.0.0/{0}/{1}.edf".format(rec_fol,rec_num))  # noqa: E501
     time,raw_data = data.times,data.get_data()
 # you can get the metadata included in the file and a list of all channels:
     info = data.info
     channels = data.ch_names
-    c_num=20
+    c_num=2
     output=raw_data[c_num]
     channel_name=channels[c_num]
     #output=signal.resample_poly(output, down=360, up=250)
@@ -51,18 +63,33 @@ def open_record(rec_num : str)->tuple:
 
 #------------records----------------------
 #PN00-1,PN00-2,PN00-3,PN00-4,PN00-5
-Y,ch_name,time=open_record("PN00-1")
+Y,ch_name,time=open_record("PN00","PN00-3")
 Y2=mov_avg_filt(Y)
-Y3=bandpass_filt(Y2)
-Y4=denoise_wavelet(Y3, wavelet='db6', mode='soft', wavelet_levels=3, method='BayesShrink', rescale_sigma='True')
+#Y3=bandpass_filt(Y2)
+Y3=denoise_wavelet(Y2, wavelet='db6', mode='soft', wavelet_levels=3, method='BayesShrink', rescale_sigma='True')
+delta,theta,alpha,beta,gamma=make_bands(Y3)
 
-
-fig, axs = plt.subplots()
+fig, axs = plt.subplots(6)
 fig.suptitle('EEG templates')
-show_wave(Y,time)
-show_wave(Y2,time)
-show_wave(Y3,time)
-show_wave(Y4,time)
+first_few,lenth=False,2000
+if first_few:
+    show_wave(Y[:lenth],time[:lenth],0)
+    show_wave(Y2[:lenth],time[:lenth],0)
+    show_wave(Y3[:lenth],time[:lenth],0)
+    show_wave(delta[:lenth],time[:lenth],1)
+    show_wave(theta[:lenth],time[:lenth],2)
+    show_wave(alpha[:lenth],time[:lenth],3)
+    show_wave(beta[:lenth],time[:lenth],4)
+    show_wave(gamma[:lenth],time[:lenth],5)
+else:
+    show_wave(Y,time,0)
+    show_wave(Y2,time,0)
+    show_wave(Y3,time,0)
 
+    show_wave(delta,time,1)
+    show_wave(theta,time,2)
+    show_wave(alpha,time,3)
+    show_wave(beta,time,4)
+    show_wave(gamma,time,5)
 
 plt.show()
