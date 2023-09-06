e3bc12e6b5f4973742713a48be3690ec88158cd7
KavinduWeerasinghe <75959908+KavinduWeerasinghe@users.noreply.github.com>
2023-09-07 00:35:40 +0530

    GUI try

--- read.py
+++ read.py
@@ -5,6 +5,7 @@ import mne
 import matplotlib.pyplot as plt
 import pywt
 from skimage.restoration import denoise_wavelet
+from GUI import make_GUI
 
 def mov_avg_filt(y,win=4):
     ret = np.cumsum(y, dtype=float)
@@ -26,13 +27,14 @@ def make_bands(Y):
     beta = bandpass_filt(Y,13,30)
     gamma = bandpass_filt(Y,30,100)
     return delta,theta,alpha,beta,gamma
-
-def show_wave(r,samples,multiple=False):
-    if multiple>=0:
-        axs[multiple].plot(samples,r)
-    else:
-        axs.plot(samples,r)
         
+def open_annotations(file,number):
+    with open(file) as f:
+        lines = f.readlines()
+        if str(number) in lines:
+            print(lines)
+    return None
+
 def open_record(rec_fol: str, rec_num : str)->tuple:
     """This function opens a given recording in NST database.
 
@@ -47,8 +49,9 @@ def open_record(rec_fol: str, rec_num : str)->tuple:
         
     database location : D:/Intership/Template matching/databases/data_NST/
     """
-    data=mne.io.read_raw_edf("F:/FYP/Sienna Scalp dataset/files/sienna-scalp-eeg/1.0.0/{0}/{1}.edf".format(rec_fol,rec_num))  # noqa: E501
+    data=mne.io.read_raw_edf("F:/FYP/CHB-MIT/chb01/chb01_01.edf")  # noqa: E501
     time,raw_data = data.times,data.get_data()
+    #open_annotations("F:/FYP/Sienna Scalp dataset/files/sienna-scalp-eeg/1.0.0/{0}/Seizures-list-{1}.txt".format(rec_fol,rec_fol),rec_num)  # noqa: E501
 # you can get the metadata included in the file and a list of all channels:
     info = data.info
     channels = data.ch_names
@@ -63,33 +66,45 @@ def open_record(rec_fol: str, rec_num : str)->tuple:
 
 #------------records----------------------
 #PN00-1,PN00-2,PN00-3,PN00-4,PN00-5
-Y,ch_name,time=open_record("PN00","PN00-3")
+Y,ch_name,time=open_record("PN01","PN01-1")
 Y2=mov_avg_filt(Y)
 #Y3=bandpass_filt(Y2)
 Y3=denoise_wavelet(Y2, wavelet='db6', mode='soft', wavelet_levels=3, method='BayesShrink', rescale_sigma='True')
 delta,theta,alpha,beta,gamma=make_bands(Y3)
 
-fig, axs = plt.subplots(6)
+fig, axs = plt.subplots(2,2)
 fig.suptitle('EEG templates')
-first_few,lenth=False,2000
-if first_few:
-    show_wave(Y[:lenth],time[:lenth],0)
-    show_wave(Y2[:lenth],time[:lenth],0)
-    show_wave(Y3[:lenth],time[:lenth],0)
-    show_wave(delta[:lenth],time[:lenth],1)
-    show_wave(theta[:lenth],time[:lenth],2)
-    show_wave(alpha[:lenth],time[:lenth],3)
-    show_wave(beta[:lenth],time[:lenth],4)
-    show_wave(gamma[:lenth],time[:lenth],5)
-else:
-    show_wave(Y,time,0)
-    show_wave(Y2,time,0)
-    show_wave(Y3,time,0)
+    
+#plot the raw EEG
+axs[0,0].plot(time,Y)
+axs[0,0].set_xlabel("time")
+axs[0,0].set_ylabel("amplitude")
 
-    show_wave(delta,time,1)
-    show_wave(theta,time,2)
-    show_wave(alpha,time,3)
-    show_wave(beta,time,4)
-    show_wave(gamma,time,5)
+#plot the alpha band
+axs[0,1].plot(time,alpha)
+axs[0,1].set_xlabel("time")
+axs[0,1].set_ylabel("amplitude")
 
-plt.show()
+#plot the beta band
+axs[1,0].plot(time,beta)
+axs[1,0].set_xlabel("time")
+axs[1,0].set_ylabel("amplitude")
+
+#plot the gamma band
+axs[1,1].plot(time,gamma)
+axs[1,1].set_xlabel("time")
+axs[1,1].set_ylabel("amplitude")
+
+#give the name of the channel
+
+plt.legend([ch_name,"moving average","denoised"])
+
+#give all the title of the subplots
+axs[0,0].set_title("Raw EEG")
+axs[0,1].set_title("Alpha band")
+axs[1,0].set_title("Beta band")
+axs[1,1].set_title("Gamma band")
+
+#show the plot in GUI
+
+make_GUI(fig)
