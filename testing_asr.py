673ff23305fa5e9dcf4ec308eb2d68f3b4803c41
KavinduWeerasinghe <75959908+KavinduWeerasinghe@users.noreply.github.com>
2024-01-19 15:56:41 +0530

    normalization-tested

--- testing_asr.py
+++ testing_asr.py
@@ -6,7 +6,7 @@ from Filtering import Basic_filters as bf
 from Read.Read_zenodo import open_record as open_record_z
 import time as Time
 import spkit as sp
-from scipy.fftpack import fft,ifft
+from scipy.signal import periodogram as psd
 
 def applyasr(X_5min,fs=250):
     pre_cleaned, _ = clean_windows(X_5min, fs, max_bad_chans=0.7)
@@ -48,8 +48,6 @@ axs1.set_xlabel("Time (s)")
 axs1.set_ylabel("Amplitude")
 axs1.grid(True)
 
-plt.show()
-
 samples=len(output)
 #getting all of the outputs
 X=np.empty((len(Channels),samples))
@@ -65,24 +63,38 @@ print(X.shape)
 
 X=np.array(X)
 
-X=X*(10000000)
+#X=X*(3000000)
 
 #nomralizing X
-#X_norm=np.linalg.norm(X,axis=0)
-#X=X/X_norm
+X_mus=np.mean(X,axis=1)
+X_sigmas=np.std(X,axis=1)
+X=(X-np.array([X_mus]).T)/np.array([X_sigmas]).T
+
+#plot the signal after normalization
+fig3, axs3 = plt.subplots()
+axs3.plot(time,X[0,:])
+axs3.set_title("Normalized Signal")
+axs3.set_xlabel("Time (s)")
+axs3.set_ylabel("Amplitude")
+axs3.grid(True)
+plt.show()
 
 start0=300
-seconds=15
+seconds=5
 minutes=1
 start0=start0*fs
 X_5min=X[:,start0:start0+minutes*seconds*fs]
 time_5min=time[start0:start0+minutes*seconds*fs]
 clean_array=bf.appy_basics(X_5min,btype='highpass')
 clean_array_new = clean_array.T
-clean_array_new= sp.eeg.ATAR(clean_array_new.copy(),wv='db4', winsize=128, beta=0.5,thr_method='ipr',OptMode='soft', verbose=0)
+beta_val=0.3
+k_1=0.01
+k_2=1
+clean_array_new= sp.eeg.ATAR(clean_array_new.copy(), wv='db3', winsize=128, beta=beta_val, thr_method='ipr', OptMode='soft', verbose=0,k1=k_1,k2=k_2)
 clean_array_new=clean_array_new.T
-X_fft=fft(X_5min)
-clean_array_fft=fft(clean_array)
+fx_psd,X_psd=psd(X_5min,fs=250)
+f_clean_psd,clean_array_psd=psd(clean_array,fs=250)
+f_new_psd,clean_array_new_psd=psd(clean_array_new,fs=250)
 #clean_array=applyasr(X_5min,fs)
 
 plt.ion()
@@ -92,75 +104,81 @@ fig2, axs2 = plt.subplots(5,2)
 #plotting the 19 channels
 print(X_5min.shape,clean_array.shape)
 
-lin1,=axs2[0,0].plot(time_5min,X_5min[0,:],color="blue")
-lin2,=axs2[0,0].plot(time_5min,clean_array[0,:],color="red")
-lin3,=axs2[1,0].plot(time_5min,X_5min[1,:],color="blue")
-lin4,=axs2[1,0].plot(time_5min,clean_array[1,:],color="red")
-lin5,=axs2[2,0].plot(time_5min,X_5min[2,:],color="blue")
-lin6,=axs2[2,0].plot(time_5min,clean_array[2,:],color="red")
-lin7,=axs2[3,0].plot(time_5min,X_5min[3,:],color="blue")
-lin8,=axs2[3,0].plot(time_5min,clean_array[3,:],color="red")
-lin9,=axs2[4,0].plot(time_5min,X_5min[4,:],color="blue")
-lin10,=axs2[4,0].plot(time_5min,clean_array[4,:],color="red")
-lin21,=axs2[0,0].plot(time_5min,clean_array_new[0,:],color="green")
-lin22,=axs2[1,0].plot(time_5min,clean_array_new[1,:],color="green")
-lin23,=axs2[2,0].plot(time_5min,clean_array_new[2,:],color="green")
-lin24,=axs2[3,0].plot(time_5min,clean_array_new[3,:],color="green")
-lin25,=axs2[4,0].plot(time_5min,clean_array_new[4,:],color="green")
+channel_list=[1,3,5,7,9]
 
-#plotting the fft of the signals between 1 and 80 Hz
-lin11,=axs2[0,1].plot(np.abs(X_fft[0,:]),color="blue")
-lin12,=axs2[1,1].plot(np.abs(X_fft[1,:]),color="blue")
-lin13,=axs2[2,1].plot(np.abs(X_fft[2,:]),color="blue")
-lin14,=axs2[3,1].plot(np.abs(X_fft[3,:]),color="blue")
-lin15,=axs2[4,1].plot(np.abs(X_fft[4,:]),color="blue")
-lin16,=axs2[0,1].plot(np.abs(clean_array_fft[0,:]),color="red")
-lin17,=axs2[1,1].plot(np.abs(clean_array_fft[1,:]),color="red")
-lin18,=axs2[2,1].plot(np.abs(clean_array_fft[2,:]),color="red")
-lin19,=axs2[3,1].plot(np.abs(clean_array_fft[3,:]),color="red")
-lin20,=axs2[4,1].plot(np.abs(clean_array_fft[4,:]),color="red")
-axs2[0,1].set_xlim([0,80])
-axs2[1,1].set_xlim([0,80])
-axs2[2,1].set_xlim([0,80])
-axs2[3,1].set_xlim([0,80])
-axs2[4,1].set_xlim([0,80])
+lin1,=axs2[0,0].plot(time_5min,X_5min[channel_list[0],:],color="blue")
+lin2,=axs2[0,0].plot(time_5min,clean_array[channel_list[0],:],color="red")
+lin3,=axs2[1,0].plot(time_5min,X_5min[channel_list[1],:],color="blue")
+lin4,=axs2[1,0].plot(time_5min,clean_array[channel_list[1],:],color="red")
+lin5,=axs2[2,0].plot(time_5min,X_5min[channel_list[2],:],color="blue")
+lin6,=axs2[2,0].plot(time_5min,clean_array[channel_list[2],:],color="red")
+lin7,=axs2[3,0].plot(time_5min,X_5min[channel_list[3],:],color="blue")
+lin8,=axs2[3,0].plot(time_5min,clean_array[channel_list[3],:],color="red")
+lin9,=axs2[4,0].plot(time_5min,X_5min[channel_list[4],:],color="blue")
+lin10,=axs2[4,0].plot(time_5min,clean_array[channel_list[4],:],color="red")
+lin21,=axs2[0,0].plot(time_5min,clean_array_new[channel_list[0],:],color="green")
+lin22,=axs2[1,0].plot(time_5min,clean_array_new[channel_list[1],:],color="green")
+lin23,=axs2[2,0].plot(time_5min,clean_array_new[channel_list[2],:],color="green")
+lin24,=axs2[3,0].plot(time_5min,clean_array_new[channel_list[3],:],color="green")
+lin25,=axs2[4,0].plot(time_5min,clean_array_new[channel_list[4],:],color="green")
+
+#plotting the psd of the signals between 1 and 80 Hz
+lin11,=axs2[0,1].semilogy(fx_psd,np.sqrt(X_psd[channel_list[0],:]),color="blue")
+lin12,=axs2[1,1].semilogy(fx_psd,np.sqrt(X_psd[channel_list[1],:]),color="blue")
+lin13,=axs2[2,1].semilogy(fx_psd,np.sqrt(X_psd[channel_list[2],:]),color="blue")
+lin14,=axs2[3,1].semilogy(fx_psd,np.sqrt(X_psd[channel_list[3],:]),color="blue")
+lin15,=axs2[4,1].semilogy(fx_psd,np.sqrt(X_psd[channel_list[4],:]),color="blue")
+lin16,=axs2[0,1].semilogy(f_clean_psd,np.sqrt(clean_array_psd[channel_list[0],:]),color="red")
+lin17,=axs2[1,1].semilogy(f_clean_psd,np.sqrt(clean_array_psd[channel_list[1],:]),color="red")
+lin18,=axs2[2,1].semilogy(f_clean_psd,np.sqrt(clean_array_psd[channel_list[2],:]),color="red")
+lin19,=axs2[3,1].semilogy(f_clean_psd,np.sqrt(clean_array_psd[channel_list[3],:]),color="red")
+lin20,=axs2[4,1].semilogy(f_clean_psd,np.sqrt(clean_array_psd[channel_list[4],:]),color="red")
+lin30,=axs2[0,1].semilogy(f_new_psd,np.sqrt(clean_array_new_psd[channel_list[0],:]),color="green")
+lin31,=axs2[1,1].semilogy(f_new_psd,np.sqrt(clean_array_new_psd[channel_list[1],:]),color="green")
+lin32,=axs2[2,1].semilogy(f_new_psd,np.sqrt(clean_array_new_psd[channel_list[2],:]),color="green")
+lin33,=axs2[3,1].semilogy(f_new_psd,np.sqrt(clean_array_new_psd[channel_list[3],:]),color="green")
+lin34,=axs2[4,1].semilogy(f_new_psd,np.sqrt(clean_array_new_psd[channel_list[4],:]),color="green")
+for i in range(5):
+    axs2[i,1].set_xlim([1,80])
+    axs2[i,1].set_ylim([10**(-4),1])
 
 for start in range(start0+1,start0+1400000,fs):
     X_5min=X[:,start:start+minutes*seconds*fs]
     time_5min=time[start:start+minutes*seconds*fs]
     clean_array=bf.appy_basics(X_5min,btype='highpass')
     clean_array_new = clean_array.T
-    clean_array_new = sp.eeg.ATAR(clean_array_new.copy(),wv='db4', winsize=128, beta=0.5,thr_method='ipr',OptMode='soft', verbose=0)
+    clean_array_new = sp.eeg.ATAR(clean_array_new.copy(),wv='db4', winsize=128, beta=beta_val,thr_method='ipr',OptMode='soft', verbose=0,k1=k_1,k2=k_2)
     clean_array_new=clean_array_new.T
-    X_fft=fft(X_5min)
-    clean_array_fft=fft(clean_array)
+    fx_psd,X_psd=psd(X_5min,fs=250)
+    f_clean_psd,clean_array_psd=psd(clean_array,fs=250)
+    f_new_psd,clean_array_new_psd=psd(clean_array_new,fs=250)
     #clean_array=applyasr(X_5min,fs)
 
     #delay componsate for 0.25 seconds
 
     #updating the data values
-    plus,minus=np.min(X)/100,np.max(X)/100
+    plus,minus=-2.5,2.5
     #plus,minus=1,-1
-    lin1.set_ydata(X_5min[0,:])
-    lin2.set_ydata(clean_array[0,:])
+    lin1.set_ydata(X_5min[channel_list[0],:])
+    lin2.set_ydata(clean_array[channel_list[0],:])
     axs2[0,0].set_ylim([plus,minus])
-    lin3.set_ydata(X_5min[1,:])
-    lin4.set_ydata(clean_array[1,:])
+    lin3.set_ydata(X_5min[channel_list[1],:])
+    lin4.set_ydata(clean_array[channel_list[1],:])
     axs2[1,0].set_ylim([plus,minus])
-    lin5.set_ydata(X_5min[2,:])
-    lin6.set_ydata(clean_array[2,:])
+    lin5.set_ydata(X_5min[channel_list[2],:])
+    lin6.set_ydata(clean_array[channel_list[2],:])
     axs2[2,0].set_ylim([plus,minus])
-    lin7.set_ydata(X_5min[3,:])
-    lin8.set_ydata(clean_array[3,:])
+    lin7.set_ydata(X_5min[channel_list[3],:])
+    lin8.set_ydata(clean_array[channel_list[3],:])
     axs2[3,0].set_ylim([plus,minus])
-    lin9.set_ydata(X_5min[4,:])
-    lin10.set_ydata(clean_array[4,:])
+    lin9.set_ydata(X_5min[channel_list[4],:])
+    lin10.set_ydata(clean_array[channel_list[4],:])
     axs2[4,0].set_ylim([plus,minus])
-    lin21.set_ydata(clean_array_new[0,:])
-    lin22.set_ydata(clean_array_new[1,:])
-    lin23.set_ydata(clean_array_new[2,:])
-    lin24.set_ydata(clean_array_new[3,:])
-    lin25.set_ydata(clean_array_new[4,:])
+    lin21.set_ydata(clean_array_new[channel_list[0],:])
+    lin22.set_ydata(clean_array_new[channel_list[1],:])
+    lin23.set_ydata(clean_array_new[channel_list[2],:])
+    lin24.set_ydata(clean_array_new[channel_list[3],:])
+    lin25.set_ydata(clean_array_new[channel_list[4],:])
     lin1.set_xdata(time_5min)
     lin2.set_xdata(time_5min)
     lin3.set_xdata(time_5min)
@@ -179,17 +197,25 @@ for start in range(start0+1,start0+1400000,fs):
     for i in range(5):
         axs2[i,0].set_xlim([time_5min[0],time_5min[-1]])
 
-    #plotting the fft of the signals between 1 and 80 Hz
-    lin11.set_ydata(np.abs(X_fft[0,:]))
-    lin12.set_ydata(np.abs(X_fft[1,:]))
-    lin13.set_ydata(np.abs(X_fft[2,:]))
-    lin14.set_ydata(np.abs(X_fft[3,:]))
-    lin15.set_ydata(np.abs(X_fft[4,:]))
-    lin16.set_ydata(np.abs(clean_array_fft[0,:]))
-    lin17.set_ydata(np.abs(clean_array_fft[1,:]))
-    lin18.set_ydata(np.abs(clean_array_fft[2,:]))
-    lin19.set_ydata(np.abs(clean_array_fft[3,:]))
-    lin20.set_ydata(np.abs(clean_array_fft[4,:]))
+    #plotting the psd of the signals between 1 and 80 Hz
+    lin11.set_ydata(np.sqrt(X_psd[channel_list[0],:]))
+    lin12.set_ydata(np.sqrt(X_psd[channel_list[1],:]))
+    lin13.set_ydata(np.sqrt(X_psd[channel_list[2],:]))
+    lin14.set_ydata(np.sqrt(X_psd[channel_list[3],:]))
+    lin15.set_ydata(np.sqrt(X_psd[channel_list[4],:]))
+    lin16.set_ydata(np.sqrt(clean_array_psd[channel_list[0],:]))
+    lin17.set_ydata(np.sqrt(clean_array_psd[channel_list[1],:]))
+    lin18.set_ydata(np.sqrt(clean_array_psd[channel_list[2],:]))
+    lin19.set_ydata(np.sqrt(clean_array_psd[channel_list[3],:]))
+    lin20.set_ydata(np.sqrt(clean_array_psd[channel_list[4],:]))
+    lin30.set_ydata(np.sqrt(clean_array_new_psd[channel_list[0],:]))
+    lin31.set_ydata(np.sqrt(clean_array_new_psd[channel_list[1],:]))
+    lin32.set_ydata(np.sqrt(clean_array_new_psd[channel_list[2],:]))
+    lin33.set_ydata(np.sqrt(clean_array_new_psd[channel_list[3],:]))
+    lin34.set_ydata(np.sqrt(clean_array_new_psd[channel_list[4],:]))
+    for i in range(5):
+        axs2[i,1].set_xlim([1,80])
+        axs2[i,1].set_ylim([10**(-4),10])
 
     fig2.canvas.draw()
     fig2.canvas.flush_events()
